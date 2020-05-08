fb46b397b46186939f36262a5facf49fa9c7f0a7
djuka10 <viktordjuka10@gmail.com>
2020-05-08 14:54:09 +0200

    Minor changes

--- FindARoommateAndroid/app/src/main/java/rs/ac/uns/ftn/findaroommate/activity/SearchActivity.java
+++ FindARoommateAndroid/app/src/main/java/rs/ac/uns/ftn/findaroommate/activity/SearchActivity.java
@@ -1,11 +1,16 @@
 package rs.ac.uns.ftn.findaroommate.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.NumberPicker;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import rs.ac.uns.ftn.findaroommate.R;
+import rs.ac.uns.ftn.findaroommate.model.Ad;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -22,6 +27,22 @@ public class SearchActivity extends AppCompatActivity {
 
         npMax.setMinValue(100);
         npMax.setMaxValue(500);
+
+        Button adView = (Button) findViewById(R.id.btn_apply);
+        adView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SearchActivity.this, "MasterDetailView action", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(SearchActivity.this, RoomListActivity.class);
+
+                RoomListActivity.adsList = Ad.getAllAds();
+                for (Ad ad: RoomListActivity.adsList) {
+                    RoomListActivity.adsMap.put(ad.getId().toString(), ad);
+                }
+
+                startActivity(intent);
+            }
+        });
     }
 
 }
