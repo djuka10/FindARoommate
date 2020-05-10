d5374b768267274b62bb54d0bb81252159661ccf
djuka10 <viktordjuka10@gmail.com>
2020-05-10 16:11:56 +0200

    minor changes

--- FindARoommateAndroid/app/src/main/java/rs/ac/uns/ftn/findaroommate/MainActivity.java
+++ FindARoommateAndroid/app/src/main/java/rs/ac/uns/ftn/findaroommate/MainActivity.java
@@ -23,10 +23,14 @@ import rs.ac.uns.ftn.findaroommate.activity.ProfileFormActivity;
 import rs.ac.uns.ftn.findaroommate.activity.SignUpActivity;
 import rs.ac.uns.ftn.findaroommate.activity.SignUpHomeActivity;
 import rs.ac.uns.ftn.findaroommate.model.Ad;
+import rs.ac.uns.ftn.findaroommate.model.Language;
 import rs.ac.uns.ftn.findaroommate.model.Message;
 import rs.ac.uns.ftn.findaroommate.model.ResourceRegistry;
+import rs.ac.uns.ftn.findaroommate.model.User;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Boolean initDataFlag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,47 +39,10 @@ public class MainActivity extends AppCompatActivity {
 
         ActiveAndroid.initialize(this);
 
-       /* Ad ad = new Ad().builder().build();
-        ad.setFlatM2(25);
-        ad.setAdType("type");
-        ad.setAvailableFrom(new Date());
-        ad.setAvailableUntil(new Date());
-        ad.setBoysNum(3);
-        ad.setCostsIncluded(true);
-        ad.setDeposit(300);
-        ad.setDescription("Brutalna stancuga u centru grada");
-        ad.setLadiesNum(2);
-        ad.setLatitude(25);
-        ad.setLongitude(25);
-        ad.setMaxPerson(5);
-        ad.setMinDays(34);
-        ad.setPrice(322);
-        ad.setRoomM2(20);
-        ad.setTitle("Rajski pogled!");
-        ad.save();
+        initDataFlag = false;
 
-        Ad ad2 = Ad.getRandom();
-        ad2.setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
-        ad2.setTitle("Apartment with positive view");
-        String pattern = "MM/dd/yyyy";
-        DateFormat df = new SimpleDateFormat(pattern);
-        Date dateFrom = new Date();
-
-        ad2.setAvailableFrom(new Date());
-        ad2.setAvailableUntil(new Date());
-        ad2.save();*/
-       //45.209217
-
-/*        Ad ad2 = Ad.getRandom();
-        ad2.setLongitude((float) 48.209217);
-        ad2.setLatitude((float) 29.722242);
-        ad2.save();*/
-
-/*        ResourceRegistry rr = new ResourceRegistry().builder().build();
-        rr.setAddId(1);
-        rr.setProfilePicture(false);
-        rr.setUri(R.drawable.apartment1);
-        rr.save();*/
+        if(initDataFlag)
+            initData();
 
         List<Ad> storedItems = Ad.getAllAds();
 
@@ -103,4 +70,123 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    private void initData() {
+
+        Ad ad = new Ad().builder().build();
+        ad.setFlatM2(25);
+        ad.setAdType("type");
+        ad.setAvailableFrom(new Date());
+        ad.setAvailableUntil(new Date());
+        ad.setBoysNum(3);
+        ad.setCostsIncluded(true);
+        ad.setDeposit(300);
+        ad.setDescription("Brutalna stancuga u centru grada");
+        ad.setLadiesNum(2);
+        ad.setLatitude(25);
+        ad.setLongitude(25);
+        ad.setMaxPerson(5);
+        ad.setMinDays(34);
+        ad.setPrice(322);
+        ad.setRoomM2(20);
+        ad.setTitle("Rajski pogled!");
+        ad.save();
+
+        Ad ad2 = Ad.getRandom();
+        ad2.setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
+        ad2.setTitle("Apartment with positive view");
+        String pattern = "MM/dd/yyyy";
+        DateFormat df = new SimpleDateFormat(pattern);
+        Date dateFrom = new Date();
+
+        ad2.setAvailableFrom(new Date());
+        ad2.setAvailableUntil(new Date());
+        ad2.save();
+
+        Ad ad3 = Ad.getRandom();
+        ad3.setLongitude((float) 48.209217);
+        ad3.setLatitude((float) 29.722242);
+        ad3.save();
+
+      /*  ResourceRegistry rr = new ResourceRegistry().builder().build();
+        rr.setAddId(1);
+        rr.setProfilePicture(false);
+        rr.setUri(R.drawable.apartment1);
+        rr.save();*/
+
+        //Languages
+        Language l1 = new Language("SRB","Serbian");
+        Language l2 = new Language("ENG","English");
+        Language l3 = new Language("ESP","Spanish");
+        Language l4 = new Language("PRT","Portuguese");
+        Language l5 = new Language("FRA","French");
+        Language l6 = new Language("GER","German");
+        Language l7 = new Language("HRV","Croatian");
+        Language l8 = new Language("BGR","Bulgarian");
+        Language l9 = new Language("ITA","Italian");
+
+        l1.save();
+        l2.save();
+        l3.save();
+        l4.save();
+        l5.save();
+        l6.save();
+        l7.save();
+        l8.save();
+        l9.save();
+
+
+        //users
+        User userLuka = new User().builder()
+                .firstName("Luka")
+                .lastName("Jovanovic")
+                .email("lukajvnv@gmail.com")
+                .birthDay(new Date(1996,3,29))
+                .gender("male")
+                .about("Lorem ipsum")
+                .password("password")
+                .occupation("student")
+                .studyLevel("master")
+                .workingStatus("unemployed")
+                .urlProfile("url")
+                .activeSince(new Date())
+                .build();
+
+        User userPop = new User().builder()
+                .firstName("Srdjan")
+                .lastName("Popovic")
+                .email("srdjanpopovic14@gmail.com")
+                .birthDay(new Date(1996,3,1))
+                .gender("male")
+                .about("Lorem ipsum")
+                .password("password")
+                .occupation("student")
+                .studyLevel("4 year")
+                .workingStatus("unemployed")
+                .urlProfile("url")
+                .activeSince(new Date())
+                .build();
+
+        User userDjuka = new User().builder()
+                .firstName("Viktor")
+                .lastName("Djuka")
+                .email("viktordjuka10@gmail.com")
+                .birthDay(new Date(1996,2,9))
+                .gender("male")
+                .about("Lorem ipsum")
+                .password("password")
+                .occupation("student")
+                .studyLevel("master")
+                .workingStatus("unemployed")
+                .urlProfile("url")
+                .activeSince(new Date())
+                .build();
+
+        userDjuka.save();
+        userLuka.save();
+        userPop.save();
+
+
+    }
+
 }
